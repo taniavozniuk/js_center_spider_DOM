a67c6643996577a965ec2796b5257649220f56a4
Tetiana Vozniuk <taniavoznyk19655@gmail.com>
2024-11-21 10:34:21 +0200

    make build

---
+++ index.c2db8bab.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".spider"),e=document.querySelector(".wall"),o=e.offsetWidth,s=e.offsetHeight,f=t.offsetWidth,l=t.offsetHeight;t.style.position="absolute",t.style.top="".concat((o-f)/2,"px"),t.style.left="".concat((s-l)/2,"px");
+//# sourceMappingURL=index.c2db8bab.js.map
